6be38027c9cf1294bb95be924ce241545cbf5be8
Борисов Илья <ilya.borisov@technopark.ru>
2025-02-18 00:01:34 +0300

    Init Web App

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+useHead({
+  script: [
+    { src: 'https://telegram.org/js/telegram-web-app.js?56', defer: true },
+  ]
+})
+</script>
+
+<template>
+  <main>
+    <ClientOnly>
+      <slot />
+    </ClientOnly>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <section>
+    <h1>Hello Web Mini App</h1>
+  </section>
+</template>
